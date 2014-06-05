d3b5da21e9fbea42c106acdc73f071055e84ffd6
Khemraj Thapa <khyamay@gmail.com>
2014-06-06 00:45:33 +0100

    Initial Scaffolding

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+	fs = require("fs"),
+	port = process.env.PORT || 2595;
+
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.set("view options",{
+	layout: false
+});
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res){
+	res.render('public/index.html');
+
+});
+
+app.listen(port);
+console.log('Express server running at http://localhost:' + port);
